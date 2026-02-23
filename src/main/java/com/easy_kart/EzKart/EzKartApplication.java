966708cd37971984e6af58b7d6b6aeadfa7e51a4
manjunath25 <k.mnjunth@gmail.com>
2026-02-23 17:31:49 +0530

    Initial commit

---
+++ src/main/java/com/easy_kart/EzKart/EzKartApplication.java
@@ -0,0 +1,13 @@
+package com.easy_kart.EzKart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzKartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzKartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easy_kart/EzKart/EzKartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easy_kart.EzKart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzKartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
